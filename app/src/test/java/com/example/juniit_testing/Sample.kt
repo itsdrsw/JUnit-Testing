83fbc8376799931e2dcffbaca58df644bcf40e1b
Dimas Dharma Setiawan <marswan27@gmail.com>
2023-11-02 14:56:53 +0700

    Praktik testing kedua jika fail
    Pratik testing ini dilakukan berdasarkan referensi https://kotlinlang.org/docs/jvm-test-using-junit.html#create-a-test. Namun kali ini saya mencoba jika hasilnya fail

---
+++ app/src/test/java/com/example/juniit_testing/Sample.kt
@@ -0,0 +1,8 @@
+package com.example.juniit_testing
+
+class Sample { //deklarasi sebuah kelas Kotlin bernama Sample
+
+    fun sum(a: Int, b: Int): Int { // membuat method dan mennggunakan dua parameter bertipe Int (bilangan bulat) yang dinamai a dan b
+        return a + b // mengembalikan hasil penjumlahan dari parameter a dan b
+    }
+}
---
+++ app/src/test/java/com/example/juniit_testing/SampleTest.kt
@@ -0,0 +1,20 @@
+package com.example.juniit_testing
+
+import org.junit.Assert //mengimpor kelas-kelas yang diperlukan dari pustaka JUnit
+import org.junit.Test //mengimpor anotasi Test dari pustaka JUnit
+
+internal class SampleTest { //mendeklarasikan kelas dan keyword 'internal' berarti kelas ini dapat diakses oleh kode dalam modul yang sama
+    private val testSample: Sample = Sample() //membuat objek Sample() dari kelas Sample
+
+    @Test //anotasi method pengujian
+    fun testSum(){ //method ini melakukan pengujian pada method sum dari kelas Sample.
+        val expected = 90 //inisiasi variabel expected
+        Assert.assertEquals(expected, testSample.sum(80, 2))
+        /**
+        pernyataan pengujian aktual, method assertEquals dari kelas Assert digunakan untuk
+        memeriksa apakah hasil yang diharapkan (expected) sama dengan hasil yang dikembalikan
+        oleh pemanggilan method testSample.sum(80, 2). Jika hasilnya sama, pengujian akan lulus.
+        Jika tidak, pengujian akan gagal.
+         */
+    }
+}
